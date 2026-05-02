3c54c0002d6fd5bb693ad96b0b4b4e14713f17ad
Waseem <waseeme900@gmail.com>
2026-05-02 14:37:15 +0300

    cange config file

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,3 @@
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
@@ -6,7 +5,8 @@ from typing import Optional
 class Setting(BaseSettings):
     DATABASE_URL: str
     DATABASE_URL_PROD: Optional[str] = None
-    IS_DEV: bool = True  
+    IS_DEV: bool = True
+
     @property
     def db_url(self) -> str:
         return self.DATABASE_URL if self.IS_DEV else self.DATABASE_URL_PROD
@@ -16,18 +16,17 @@ class Setting(BaseSettings):
     AUTH0_DOMAIN: str
     AUTH0_AUDIENCE: str
 
-    MLFLOW_MODEL_URI: str
-    BENTO_MODEL_NAME : str
-    MLFLOW_TRACKING_URI: str
+    MLFLOW_MODEL_URI: Optional[str] = None
+    BENTO_MODEL_NAME: Optional[str] = None
+    MLFLOW_TRACKING_URI: Optional[str] = None
 
-    BENTOML_MODEL_TAG: str
-    BENTO_URL: str
+    BENTOML_MODEL_TAG: Optional[str] = None
+    BENTO_URL: Optional[str] = None
 
-    GOOGLE_APPLICATION_CREDENTIALS: str
+    GOOGLE_APPLICATION_CREDENTIALS: Optional[str] = None
+    OPENAI_API_KEY: Optional[str] = None
 
-    OPENAI_API_KEY: str
-
-    ALLOW_ORIGINS: str
+    ALLOW_ORIGINS: str 
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
